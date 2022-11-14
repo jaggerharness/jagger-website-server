302aa547f3338cf49fa3460b62b91da563ec77f6
Jagger <jagger@aceonetechnologies.com>
2022-11-14 10:47:59 -0600

    new fetch routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/fetchProjects", function (req, res, next) {
+  const project = {
+    project_title: 'My First Project',
+    project_description: 'This is a sample project description. Do you like it?',
+    project_link: '/',
+  };
+  const project2 = {
+    project_title: 'My Second Project',
+    project_description: 'This is a sample second project description. Do you like it?',
+    project_link: '/',
+  };
+  let projects = [project, project2];
+  res.send(JSON.stringify({projects: projects}));
+});
+
 module.exports = router;
