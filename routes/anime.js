84896bcea9c5d2774db8840bd606cac81b3a8be4
jaggerharness <jaggerharness5@gmail.com>
2022-11-23 23:32:29 -0600

    Error handling

--- routes/anime.js
+++ routes/anime.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 var sqlite3 = require("sqlite3").verbose();
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
@@ -10,30 +10,46 @@ const DBSOURCE = "db.sqlite";
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const db_data = await prisma.anime_quotes.findMany();
-  console.log(db_data);
-  const response = await axios.get("https://animechan.vercel.app/api/random");
-  let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message);
-      throw err;
-    } else {
-      db.run(
-        `INSERT INTO anime_quotes (title, character, quote)
-            VALUES (${JSON.stringify(response.data.anime)}, ${JSON.stringify(response.data.character)}, ${JSON.stringify(response.data.quote)});`,
-        (err) => {
-          if (err) {
-            // Log err
-            console.error(err.message);
-          } else {
-            // Table created
-            console.log("Insert successful");
-          }
+  try {
+    const response = await axios.get("https://animechan.vercel.app/api/random");
+    let db = new sqlite3.Database(DBSOURCE, (err) => {
+      if (err) {
+        // Cannot open database
+        console.error(err.message);
+        throw err;
+      } else {
+        try {
+          prisma.anime_quotes.create({
+            data: {
+              title: JSON.stringify(response.data.anime),
+              character: JSON.stringify(response.data.character),
+              quote: JSON.stringify(response.data.quote),
+            },
+          });
+          console.log("Insert successful");
+        } catch (e) {
+          console.log(e.message);
         }
-      );
-    }
-  });
+        // db.run(
+        //   `INSERT INTO anime_quotes (title, character, quote)
+        //     VALUES (${JSON.stringify(response.data.anime)}, ${JSON.stringify(
+        //     response.data.character
+        //   )}, ${JSON.stringify(response.data.quote)});`,
+        //   (err) => {
+        //     if (err) {
+        //       // Log err
+        //       console.error(err.message);
+        //     } else {
+        //       // Table created
+        //       console.log("Insert successful");
+        //     }
+        //   }
+        // );
+      }
+    });
+  } catch (error) {
+    res.render("error", { message: "Error encountered", error: error });
+  }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -9,8 +12,7 @@ router.get("/", function (req, res, next) {
 router.get("/fetchProjects", function (req, res, next) {
   const project = {
     project_title: "Anime Quote Feed",
-    project_description:
-      `The following project recieves data from an anime quote API. The data is fetched at random time intervals on the server and stored in a database for display here.
+    project_description: `The following project recieves data from an anime quote API. The data is fetched at random time intervals on the server and stored in a database for display here.
       I was able to practice many basic REST concepts during the creation of this project.`,
     project_link: "/anime-feed",
   };
@@ -18,6 +20,11 @@ router.get("/fetchProjects", function (req, res, next) {
   let projects = [project];
 
   res.send(JSON.stringify({ projects: projects }));
+});
+
+router.get("/fetchFeed", async function (req, res, next) {
+  const feed_data = await prisma.anime_quotes.findMany();
+  res.send(JSON.stringify({ feed: feed_data }));
 });
 
 module.exports = router;
